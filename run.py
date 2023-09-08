f642d0984526485607648cb312f76e9498d51fa3
root <45777292@qq.com>
2023-09-08 15:35:24 +0800

    Merge branch 'origin/second'

--- run.py
+++ run.py
@@ -6,28 +6,26 @@ from server.backend import Backend_Api
 from server.babel import create_babel
 from json import load
 from flask import Flask
+import logging
+from gapp import app
+import datetime
+from gevent import pywsgi
 
-if __name__ == '__main__':
-
+# app = Flask(__name__)
+if __name__ == "__main__":
     # Load configuration from config.json
     config = load(open('config.json', 'r'))
     site_config = config['site_config']
     url_prefix = config.pop('url_prefix')
-
-    # Create the app
-    app = Flask(__name__)
     app.secret_key = secrets.token_hex(16)
-
-    # Set up Babel
     create_babel(app)
-
     # Set up the website routes
     site = Website(bp, url_prefix)
     for route in site.routes:
         bp.add_url_rule(
             route,
-            view_func=site.routes[route]['function'],
-            methods=site.routes[route]['methods'],
+            view_func=site.routes[route]["function"],
+            methods=site.routes[route]["methods"],
         )
 
     # Set up the backend API routes
@@ -35,14 +33,21 @@ if __name__ == '__main__':
     for route in backend_api.routes:
         bp.add_url_rule(
             route,
-            view_func=backend_api.routes[route]['function'],
-            methods=backend_api.routes[route]['methods'],
+            view_func=backend_api.routes[route]["function"],
+            methods=backend_api.routes[route]["methods"],
         )
 
-    # Register the blueprint
+    # Create the app and register the blueprint
+
     app.register_blueprint(bp, url_prefix=url_prefix)
+    #app.secret_key = "your_12321312312_key"  # 设置一个用于加密Session的密钥
+    app.permanent_session_lifetime = datetime.timedelta(seconds=120 * 60)
+    app.logger.setLevel(logging.INFO)
 
     # Run the Flask server
+    # print(f"Running on {site_config['port']}{url_prefix}")
+    # app.run(**site_config)
+    # print(f"Closing port {site_config['port']}")
+    server = pywsgi.WSGIServer(("0.0.0.0", 8005), app)
     print(f"Running on {site_config['port']}{url_prefix}")
-    app.run(**site_config)
-    print(f"Closing port {site_config['port']}")
+    server.serve_forever()
--- server/website.py
+++ server/website.py
@@ -3,6 +3,39 @@ from flask_babel import refresh
 from time import time
 from os import urandom
 from server.babel import get_locale, get_languages
+import functools
+import logging
+from gapp import app
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if "user_id" not in session:
+            return redirect("/login")
+            # url = url_for('login')
+            # print(url)
+            # return redirect(url)
+        return view(*args, **kwargs)
+
+    return wrapped_view
+
+
+def load_login_details():
+    login_details = {}
+    try:
+        with open("login_details.txt", "r") as file:
+            for line in file:
+                username, password = line.strip().split(":")
+                login_details[username] = password
+    except FileNotFoundError:
+        print("Login details file not found.")
+    return login_details
+
+
+def authenticate(username, password):
+    login_details = load_login_details()
+    return username in login_details and login_details[username] == password
 
 
 class Website:
@@ -14,6 +47,7 @@ class Website:
                 'function': lambda: redirect(url_for('._index')),
                 'methods': ['GET', 'POST']
             },
+            "/login": {"function": self._login, "methods": ["GET", "POST"]},
             '/chat/': {
                 'function': self._index,
                 'methods': ['GET', 'POST']
@@ -33,15 +67,62 @@ class Website:
             '/get-languages': {
                 'function': self.get_languages,
                 'methods': ['GET']
-            }
+            },
+            "/logout": {"function": self._logout, "methods": ["GET", "POST"]},
         }
 
+    def _logout():
+        session.pop("user_id", None)
+        return redirect("/login")
+
+    def _login(self):
+        if request.method == "POST":
+            user_id = request.form["user_id"]
+            password = request.form.get("password")
+
+            if not user_id or not password:
+                error_message = "Please fill in all the required fields."
+                return render_template("login.html", error=error_message)
+
+            if not authenticate(user_id, password):
+                error_message = "Invalid login details. Please try again."
+                return render_template("login.html", error=error_message)
+            session["user_id"] = user_id
+            # conversations[user_id] = initialize_conversation()
+            log_file = f"log/{user_id}.log"
+            handler = logging.FileHandler(log_file, encoding="utf-8")
+            handler.setLevel(logging.INFO)
+            formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+            handler.setFormatter(formatter)
+            message = "登陆"
+            logmes = f"{user_id}: {message}"
+
+            app.logger.addHandler(handler)
+            app.logger.info(logmes)
+            app.logger.removeHandler(handler)
+
+            # lock = threading.Lock()
+
+            # def write_log(message):
+            #     with lock:
+            #         logger = logging.getLogger()
+            #         logger.addHandler(handler)
+            #         logger.info(message)
+            #         logger.removeHandler(handler)
+
+            # write_log(logmes)
+
+            return redirect("/")
+        return render_template("login.html")
+
+    @login_required
     def _chat(self, conversation_id):
         if '-' not in conversation_id:
             return redirect(url_for('._index'))
 
         return render_template('index.html', chat_id=conversation_id, url_prefix=self.url_prefix)
 
+    @login_required
     def _index(self):
         return render_template('index.html', chat_id=f'{urandom(4).hex()}-{urandom(2).hex()}-{urandom(2).hex()}-{urandom(2).hex()}-{hex(int(time() * 1000))[2:]}', url_prefix=self.url_prefix)
 
